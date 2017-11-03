c5a234d8988cf3f9cf287b0a578719dfaba62efe
Flannon Jackson <flannon@nyu.edu>
2017-11-03 10:03:11 -0400

    Set nginx::resource::upstream to django

--- code/modules/development/tct/manifests/nginx.pp
+++ code/modules/development/tct/manifests/nginx.pp
@@ -39,13 +39,53 @@ class tct::nginx (
   String $www_dir       = lookup('tct::www_dir', String, 'first'),
 ){
 
+  #file { '/run/nginx':
+  #  ensure => directory,
+  #  owner  => 'nginx',
+  #  group  => 'nginx',
+  #}
   include nginx
-  nginx::resource::server { 'tct.home.wfc':
-    www_root =>  '/var/www/html',
+  nginx::resource::upstream { 'django' :
+    ensure  => present,
+    #members => [ 'unix:///tmp/nyu.sock', ],
+    #members => [ 'unix:///run/uwsgi/nyu.sock', ],
+    members => [ 'unix:///tmp/nyu.sock', ],
   }
+
+  #file { 'uwsgi_params' :
+  #  ensure  => file,
+  #  owner   => 'nginx',
+  #  group   => 'nginx',
+  #  content => template('tct/uwsgi_params.erb'),
+  #}
+  #nginx::resource::server { '172.28.128.6/tct':
+  nginx::resource::server { 'www.tct.org':
+    # www_root => '/var/www/html/dist.prod',
+  #  proxy        => 'http://localhost:54506',
+  #uwsgi   => 'unix:/run/uwsgi/tct.sock',
+  #  uwsgi   => 'unix:/run/uwsgi/nyu.sock',
+    uwsgi   => 'django',
+    uwsgi_params => '/etc/nginx/uwsgi_params',
+  }
+
+  #nginx::resource::location { 'test' :
+  #  uwsgi    => 'django',
+  #  server   => 'www.tct.wfc',
+  #}
+
+  nginx::resource::location { '/media' :
+    location_alias => '/srv/media', 
+    server         => 'www.tct.wfc',
+  }
+
+  nginx::resource::location { '/static' :
+    location_alias => '/srv/media', 
+    server         => 'www.tct.wfc',
+  }
+
     
-  firewall { '100 allow apache access on 80' :
-    dport  => [80, 443],
+  firewall { '100 allow http and https access' :
+    dport  => [80, 443, 8080, 9000],
     proto  => tcp,
     action => accept,
   }
